46f7e12f3d03fb448ba4e629d0957e3dba60c416
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-05-31 13:14:47 +0200

    prettify login slightly and support url forwarding

--- server.py
+++ server.py
@@ -6,10 +6,11 @@ import sys
 from flask import Flask, request, redirect, session, render_template
 from werkzeug.security import check_password_hash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import DataRequired
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from urllib.parse import quote
 
 formatter = json_log_formatter.JSONFormatter()
 
@@ -28,7 +29,7 @@ else:
 
 app = Flask(__name__)
 
-with open('/data/secret_key', 'r') as f:
+with open('data/secret_key', 'r') as f:
     app.secret_key = f.read().strip()
 
 login_url = os.getenv('LOGIN_URL', None)
@@ -39,6 +40,7 @@ if not login_url:
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    next_url = HiddenField()
     submit = SubmitField('Login')
 
 user_db = {}
@@ -46,13 +48,13 @@ user_db = {}
 
 class FileChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        if event.src_path == '/data/users':
+        if event.src_path == 'data/users':
             logger.info('User database changed, reloading')
             load_user_db()
 
 def load_user_db():
     global user_db
-    with open('/data/users', 'r') as f:
+    with open('data/users', 'r') as f:
         user_db = {}
         for line in f:
             username, password = line.strip().split(':', 1)
@@ -68,22 +70,28 @@ load_user_db()
 
 event_handler = FileChangeHandler()
 observer = Observer()
-observer.schedule(event_handler, path='/data', recursive=False)
+observer.schedule(event_handler, path='data', recursive=False)
 observer.start()
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if request.method == 'GET':
+        form.next_url.data = request.args.get('next_url', "admin.frikanalen.no")
+
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
+        next_url = form.next_url.data
+
         if validate_user(username, password):
             session['username'] = form.username.data
             logger.info('User logged in', extra={'user': form.username.data})
-            return redirect(request.args.get('next') or url_for('index'))
+            return redirect(next_url)
         else:
             logger.warning('Invalid login attempt', extra={'user': form.username.data})
-            return 'Invalid username or password'
+
     return render_template('login.html', form=form)
 
 @app.route('/auth', methods=['GET'])
@@ -93,6 +101,8 @@ def auth():
         return '', 200
     else:
         logger.warning('Invalid session, redirecting to login')
+        forwarded_uri = request.headers.get('X-Forwarded-Uri')
+        redirect_url = login_url + "/?next_url=" + quote(forwarded_uri)
         return redirect('https://login.admin.frikanalen.no', code=302)
 
 @app.route('/healthz', methods=['GET'])
