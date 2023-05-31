3ce762a83aec2f8619ff527e85534e671d7843ab
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-05-31 11:58:08 +0200

    initial commit of code

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # admin-auth-service
 Microservice to authenticate users for admin dashboards
+
+This is a Python Flask application that serves as an authentication service.
+It uses Traefik's ForwardAuth mechanism to protect services behind a Traefik ingress controller.
+
+## User database
+
+This repository includes a utility script named mkhash.py that helps you generate user data in the format expected by the application.
+When you run this script, it will prompt you for a username and password.
+It will then hash the password and print out a line in the format username:passwordhash.
+You can add this line to your user database file (users).
+To run the script, use the following command: python mkhash.py.
+ 
+## Environment Variables
+
+The application uses the following environment variables:
+
+- `ENV`: The environment in which the application is running. "development" or "production"
+- `LOGIN_URL`: The URL to which the user is redirected if their session is not valid.
+
+## Deployment
+
+The application is deployed using Helm, a package manager for Kubernetes. To deploy the application, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/frikanalen/admin-auth-service.git
+cd admin-auth-service
+Install the Helm chart:
+bash
+Copy code
+helm install admin-auth-service helm-chart/admin-auth-service
+This will deploy the application with the default values. To customize the deployment, you can create a values.yaml file with your own values and pass it to the helm install command:
+
+bash
+Copy code
+helm install admin-auth-service helm-chart/admin-auth-service -f my-values.yaml
+Replace my-values.yaml with the path to your values.yaml file.
+
+Helm Configuration
+The Helm chart has the following configurable values:
+
+replicaCount: The number of replicas of the application to run.
+image.repository: The Docker image repository.
+image.tag: The Docker image tag.
+image.pullPolicy: The Docker image pull policy.
+env: The environment in which the application is running.
+loginUrl: The URL to which the user is redirected if their session is not valid.
+resources: The resources to allocate to the application.
+service.type: The type of the service.
+service.port: The port of the service.
+ingress.enabled: Whether to create an Ingress for the application.
+ingress.hostname: The hostname for the Ingress.
+secretKey: The secret key for the Flask app. If not provided, a random key will be generated.
+
+For more information on how to configure these values, see the values.yaml file in the helm-chart/admin-auth-service directory.
---
+++ server.py
@@ -0,0 +1,103 @@
+import logging
+import os
+import json_log_formatter
+
+from flask import Flask, request, redirect, session, render_template
+from werkzeug.security import check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+formatter = json_log_formatter.JSONFormatter()
+
+json_handler = logging.StreamHandler()
+json_handler.setFormatter(formatter)
+
+logger = logging.getLogger('my_json')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
+
+
+if os.getenv('ENV') == 'dev':
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO, handlers=[json_handler])
+
+app = Flask(__name__)
+
+with open('/data/secret_key', 'r') as f:
+    app.secret_key = f.read().strip()
+
+login_url = os.getenv('LOGIN_URL', None)
+if not login_url:
+    logging.error("Missing mandatory environment variable LOGIN_URL, quitting!")
+    exit(1)
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+user_db = {}
+
+
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == '/data/users':
+            logger.info('User database changed, reloading')
+            load_user_db()
+
+def load_user_db():
+    global user_db
+    with open('/data/users', 'r') as f:
+        user_db = {}
+        for line in f:
+            username, password = line.strip().split(':')
+            user_db[username] = password
+        if not user_db:
+            logger.warning('No users in database!')
+
+def validate_user(username, password):
+    return username in user_db and check_password_hash(user_db[username], password)
+
+logger.info('Loading user database')
+load_user_db()
+
+event_handler = FileChangeHandler()
+observer = Observer()
+observer.schedule(event_handler, path='/data', recursive=False)
+observer.start()
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        with open('user_db.txt', 'r') as f:
+            for line in f:
+                username, password = line.strip().split(':')
+                if form.username.data == username and check_password_hash(password, form.password.data):
+                    session['username'] = form.username.data
+                    logger.info('User logged in', extra={'user': form.username.data})
+                    return redirect(request.args.get('next') or url_for('index'))
+            logger.warning('Invalid login attempt', extra={'user': form.username.data})
+            return 'Invalid username or password'
+    return render_template('login.html', form=form)
+
+@app.route('/auth', methods=['GET'])
+def auth():
+    if 'username' in session:
+        logger.info('Valid session', extra={'user': session['username']})
+        return '', 200
+    else:
+        logger.warning('Invalid session, redirecting to login')
+        return redirect('https://login.admin.frikanalen.no', code=302)
+
+@app.route('/healthz', methods=['GET'])
+def healthz():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
